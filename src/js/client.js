b4bf74c116be3f4007e6289d848d9d5982323726
martapz <marta.g.mattioli@gmail.com>
2017-07-26 15:05:59 +0100

    added frontend logic for up and down votes and sorting

--- src/js/client.js
+++ src/js/client.js
@@ -15,82 +15,98 @@ function init() {
     }
   });
 
-  $('.votes').one('click', updateVotes);
+  $('.votes').on('click', updateVotes);
+
+  calculateScoreAndSort();
+}
+
+function calculateScoreAndSort() {
+  const menuList = $('#menu li');
+
+  menuList.each((list) => {
+    const voteButton = $(menuList[list]).children('button');
+    const upVoteButtonValue = parseInt(($($(voteButton)[0]).text()));
+    const downVoteButtonValue = parseInt(($($(voteButton)[1]).text()));
+    $(menuList[list]).attr('data-score', `${upVoteButtonValue - downVoteButtonValue}`);
+  });
+
+  menuList.sort(function (a, b) {
+    return +b.getAttribute('data-score') - +a.getAttribute('data-score');
+  }).appendTo($('#menu'));
+}
+
+function youNeedToLogInMsg() {
+  $('main').prepend('<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>You must be logged in to vote an item!</div>');
+}
+
+function cannotDoubleVoteMsg() {
+  $('main').prepend('<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>You cannot double-vote!</div>');
 }
 
 function updateVotes(e) {
   const isUserLoggedIn = parseInt($('nav li:nth-child(2)').attr('data-value'));
-
   const whatButtonDidIClick = $(e.target).attr('id');
   const inputSiblings = $(e.target).siblings('input');
   const inputSiblingUp = inputSiblings[0];
   const inputSiblingDown = inputSiblings[1];
+  const otherButton = $(e.target).siblings('button')[0];
+  const otherButtonValue = parseInt($($(e.target).siblings('button')[0]).text());
+
+  function addVoteAndSaveDb() {
+    const newValue = parseInt($(e.target).text()) + 1;
+    var restID = $('#restId')[0].innerHTML;
+    var elmID = $(e.target).siblings('div')[0].innerHTML;
+    var upvts = parseInt($(e.target).siblings('div')[1].innerHTML);
+    var dwnvts = parseInt($(e.target).siblings('div')[2].innerHTML);
+    if ($(e.target).attr('id') === 'upvote') {
+      var objToSend = {
+        id: elmID,
+        upvotes: parseInt(upvts + 1)
+      };
+    } else if ($(e.target).attr('id') === 'downvote') {
+      var objToSend = {
+        id: elmID,
+        downvotes: parseInt(dwnvts + 1)
+      };
+    }
+    $
+    .ajax({
+      url: `${window.location.origin}/restaurants/${restID}`,
+      type: 'PUT',
+      data: objToSend,
+      success: function(data) {
+        // console.log(data);
+      }
+    });
+    $(e.target).text(newValue);
+  }
 
   if(isUserLoggedIn === 1) { //if user is not logged in
-    $('main').prepend('<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>You must be logged in to vote an item!</div>');
+    youNeedToLogInMsg();
   } else if (isUserLoggedIn === 0) { //if user is logged in
     if (whatButtonDidIClick === 'upvote' && $(inputSiblingUp).attr('value') === 'false' || whatButtonDidIClick === 'downvote' && $(inputSiblingDown).attr('value') === 'false') {
-      $('main').prepend('<div class="alert alert-danger alert-dismissible fade show" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>You cannot double-vote!</div>');
+      cannotDoubleVoteMsg();
     } else {
-      const newValue = parseInt($(e.target).text()) + 1;
-      var restID = $('#restId')[0].innerHTML;
-      var elmID = $(e.target).siblings('div')[0].innerHTML;
-      var upvts = parseInt($(e.target).siblings('div')[1].innerHTML);
-      var dwnvts = parseInt($(e.target).siblings('div')[2].innerHTML);
-      if ($(e.target).attr('id') === 'upvote') {
-        var objToSend = {
-          id: elmID,
-          upvotes: parseInt(upvts + 1)
-        };
-      } else if ($(e.target).attr('id') === 'downvote') {
-        var objToSend = {
-          id: elmID,
-          downvotes: parseInt(dwnvts + 1)
-        };
-      }
-      $
-      .ajax({
-        url: `${window.location.origin}/restaurants/${restID}`,
-        type: 'PUT',
-        data: objToSend,
-        success: function(data) {
-          // console.log(data);
+      if (whatButtonDidIClick === 'upvote' && $(inputSiblingUp).attr('value') === 'true' && whatButtonDidIClick === 'downvote' && $(inputSiblingDown).attr('value') === 'true') {
+        addVoteAndSaveDb();
+        calculateScoreAndSort();
+      } else {
+        addVoteAndSaveDb();
+        $(otherButton).text(otherButtonValue - 1);
+        if (whatButtonDidIClick === 'upvote') {
+          $(inputSiblingDown).attr('value', 'true');
+          $(inputSiblingUp).attr('value', 'false');
+        } else {
+          $(inputSiblingDown).attr('value', 'false');
+          $(inputSiblingUp).attr('value', 'true');
         }
-      });
-      $(e.target).text(newValue);
+        calculateScoreAndSort();
+      }
     }
-
-    // const newValue = parseInt($(e.target).text()) + 1;
-    // var restID = $('#restId')[0].innerHTML;
-    // var elmID = $(e.target).siblings('div')[0].innerHTML;
-    // var upvts = parseInt($(e.target).siblings('div')[1].innerHTML);
-    // var dwnvts = parseInt($(e.target).siblings('div')[2].innerHTML);
-    // if ($(e.target).attr('id') === 'upvote') {
-    //   var objToSend = {
-    //     id: elmID,
-    //     upvotes: parseInt(upvts + 1)
-    //   };
-    // } else if ($(e.target).attr('id') === 'downvote') {
-    //   var objToSend = {
-    //     id: elmID,
-    //     downvotes: parseInt(dwnvts + 1)
-    //   };
-    // }
-    // $
-    // .ajax({
-    //   url: `${window.location.origin}/restaurants/${restID}`,
-    //   type: 'PUT',
-    //   data: objToSend,
-    //   success: function(data) {
-    //     // console.log(data);
-    //   }
-    // });
-    // $(e.target).text(newValue);
   }
 }
 
 function initMap() {
-  // console.log(this);
   const input = document.getElementById('pac-input');
 
 
@@ -108,8 +124,6 @@ function initMap() {
       return;
     }
 
-    // $('#pac-input').attr('value',`${place.name}`);
-
     // console.log(place);
     var newVenue = { name: place.name, restaurantId: place.place_id, websiteURL: place.website, types: place.types, priceLevel: place.price_level, rating: place.rating, address: place.formatted_address, phoneNumber: place.international_phone_number };
     $.ajax({
@@ -120,8 +134,6 @@ function initMap() {
         location.href = `${location.origin}/restaurants/${data.restaurantId}`;
       }
     });
-
-    //$.post('/restaurants/new', newVenue)
   });
 }
 
